d70ab9908c80be61cc7a3f39fe507b004839ba54
InvertedOwl <commandking123@gmail.com>
2023-02-08 15:05:00 -0700

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,34 @@
+﻿using MelonLoader;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using EPOOutline;
+using UnityEngine.InputSystem;
+
+
+namespace OptiBot
+{
+    public static class BuildInfo
+    {
+        public const string Name = "OptiBot"; // Name of the Mod.  (MUST BE SET)
+        public const string Description = "Mod for Testing"; // Description for the Mod.  (Set as null if none)
+        public const string Author = "TestAuthor"; // Author of the Mod.  (MUST BE SET)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class OptiBot : MelonMod
+    {
+        
+        public override void OnSceneWasInitialized(int buildindex, string sceneName) // Runs when a Scene has Initialized and is passed the Scene's Build Index and Name.
+        {
+            GameObject mainCamera = GameObject.Find("Main Camera");
+            Screen.SetResolution(1280, 720, true, 60);
+            mainCamera.GetComponent<PostProcessLayer>().enabled = false;
+            mainCamera.GetComponents<Outliner>()[0].enabled = false;
+            mainCamera.GetComponents<Outliner>()[2].enabled = false;
+            //MelonLogger.Msg("OnSceneWasInitialized: " + buildindex.ToString() + " | " + sceneName);
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using MelonLoader;
+
+[assembly: AssemblyTitle(OptiBot.BuildInfo.Description)]
+[assembly: AssemblyDescription(OptiBot.BuildInfo.Description)]
+[assembly: AssemblyCompany(OptiBot.BuildInfo.Company)]
+[assembly: AssemblyProduct(OptiBot.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + OptiBot.BuildInfo.Author)]
+[assembly: AssemblyTrademark(OptiBot.BuildInfo.Company)]
+[assembly: AssemblyVersion(OptiBot.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(OptiBot.BuildInfo.Version)]
+[assembly: MelonInfo(typeof(OptiBot.OptiBot), OptiBot.BuildInfo.Name, OptiBot.BuildInfo.Version, OptiBot.BuildInfo.Author, OptiBot.BuildInfo.DownloadLink)]
+[assembly: MelonColor()]
+
+// Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
